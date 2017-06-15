92ef2ea52ca9fc338457221221506e91404abaf3
Giancarlos Salas <giansalex@gmail.com>
2017-06-15 00:12:08 -0500

    Finished migrate nodejs code.

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,5 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['VERIFY_TOKEN'] = 'aValidToken';
+$app['PAGE_ACCESS_TOKEN'] = '';
--- src/controllers.php
+++ src/controllers.php
@@ -2,9 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
@@ -17,7 +14,7 @@ $app->get('/', function () use ($app) {
 $app->get('/webhook', function (Request $request) use ($app) {
     $q = $request->query;
     if ($q->get('hub.mode') === 'subscribe' &&
-        $q->get('hub.verify_token') === 'YOUR_TOKEN') {
+        $q->get('hub.verify_token') === $app['VERIFY_TOKEN']) {
         return $q->get('hub.challenge');
     } else {
         $app->abort(403, 'Failed validation. Make sure the validation tokens match.');
@@ -66,3 +63,123 @@ $app->error(function (\Exception $e, Request $request, $code) use ($app) {
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
 });
+
+function receivedMessage($event) {
+    $senderID = $event->sender->id;
+    $recipientID = $event->recipient->id;
+    $timeOfMessage = $event->timestamp;
+    $message = $event->message;
+
+    //console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    //console.log(JSON.stringify(message));
+
+    $messageId = $message->mid;
+
+    $messageText = $message->text;
+    $messageAttachments = $message->attachments;
+
+    if ($messageText) {
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the template example. Otherwise, just echo the text we received.
+        switch ($messageText) {
+            case 'generic':
+                sendGenericMessage($senderID);
+                break;
+
+            default:
+                sendTextMessage($senderID, $messageText);
+        }
+    } else if ($messageAttachments) {
+        sendTextMessage($senderID, "Message with attachment received");
+    }
+}
+
+function receivedPostback($event) {
+    $senderID = $event->sender->id;
+    $recipientID = $event->recipient->id;
+    $timeOfPostback = $event->timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback
+    // button for Structured Messages.
+    $payload = $event->postback->payload;
+
+    //console.log("Received postback for user %d and page %d with payload '%s' " + "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to
+    // let them know it was successful
+    sendTextMessage($senderID, "Postback called");
+}
+function sendTextMessage($recipientId, $messageText) {
+    $messageData = [
+        'recipient'=> [
+            'id'=> $recipientId
+        ],
+        'message'=> [
+            'text' =>  $messageText
+        ]
+    ];
+
+  callSendAPI($messageData);
+}
+function sendGenericMessage($recipientId) {
+    $messageData = [
+        'recipient'=> [
+            'id'=> $recipientId
+        ],
+        'message'=> [
+            'attachment'=> [
+                'type'=> "template",
+                'payload'=> [
+                    'template_type'=> "generic",
+                    'elements'=> [[
+                        'title'=> "rift",
+                        'subtitle'=> "Next-generation virtual reality",
+                        'item_url'=> "https://www.oculus.com/en-us/rift/",
+                        'image_url'=> "http://messengerdemo.parseapp.com/img/rift.png",
+                        'buttons'=> [[
+                            'type'=> "web_url",
+                            'url'=> "https://www.oculus.com/en-us/rift/",
+                            'title'=> "Open Web URL"
+                        ], [
+                            'type'=> "postback",
+                            'title'=> "Call Postback",
+                            'payload'=> "Payload for first bubble",
+                        ]],
+                    ], [
+                        'title'=> "touch",
+                        'subtitle'=> "Your Hands, Now in VR",
+                        'item_url'=> "https://www.oculus.com/en-us/touch/",
+                        'image_url'=> "http://messengerdemo.parseapp.com/img/touch.png",
+                        'buttons'=> [[
+                            'type'=> "web_url",
+                            'url'=> "https://www.oculus.com/en-us/touch/",
+                            'title'=> "Open Web URL"
+                        ], [
+                            'type'=> "postback",
+                            'title'=> "Call Postback",
+                            'payload'=> "Payload for second bubble",
+                        ]]
+                    ]]
+                ]
+            ]
+        ]
+    ];
+    callSendAPI($messageData);
+}
+function callSendAPI($messageData) {
+    global $app;
+    $uri = 'https://graph.facebook.com/v2.6/me/messages?access_token' . $app['PAGE_ACCESS_TOKEN'];
+    $response = \Httpful\Request::post($uri)
+        ->addHeader('Content-Type', 'application/json')
+        ->body(json_encode($messageData))
+        ->send();
+
+    if ($response->code == 200) {
+        $body = json_decode($response->raw_body);
+        $recipientId = $body->recipient_id;
+        $messageId = $body->message_id;
+        //console.log("Successfully sent generic message with id %s to recipient %s",messageId, recipientId);
+    } else {
+        // Error
+    }
+}
